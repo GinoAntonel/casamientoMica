4ee5e788774a62369ad51bf579a0bbb0ef813b02
GinoAntonel <ginoantonel.ga@gmail.com>
2019-09-25 09:46:14 -0300

    Cambios

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,20 +2,20 @@
   <v-container grid-list-md text-xs-center>
     <br/>
     <v-layout row wrap>
-      <v-flex lg6 xs12>
-        <v-flex class="img" lg12 xs12 >
+      <v-flex lg6 xs12 offset-md-3 >
+        <v-flex class="img">
           <img
           small
           :src="require('../assets/icon_ceremonia.png')" width="150px"/>
         </v-flex>
-        <v-flex class="titleee" lg12 xs12>
+        <v-flex class="titleee">
           <h1>IGLESIA</h1>
         </v-flex>
-        <v-flex class="infoo" lg12 xs12>
+        <v-flex class="infoo">
           La ceremonia se realizará el d de Noviembre de 2019 a las 16:30 horas en el Convento San Alfonso. 
           Ingresá al link debajo para recibir las indicaciones para llegar. 
         </v-flex>
-        <v-flex class="img" lg12 xs12>
+        <v-flex class="img">
           <v-btn class="ma-2" outlined rounded color="green"
           target="_blank"
           href='https://www.google.com.ar/maps/place/Convento+San+Alfonso/@-31.3048408,-64.2977287,17z/data=!3m1!4b1!4m5!3m4!1s0x94329d06c7decc7f:0x6e0f738ff3e315db!8m2!3d-31.3048408!4d-64.2955347'
@@ -23,7 +23,7 @@
         </v-flex>
       </v-flex>
 
-      <v-flex lg6 xs12>
+      <v-flex lg6 xs12 offset-md-3  >
         <v-flex class="img" >
           <img
           small
